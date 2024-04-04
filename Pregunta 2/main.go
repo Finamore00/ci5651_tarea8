92f79a7de22a25a20728e9a615e91ef5d22e3567
Santiago Finamore <18-10125@usb.ve>
2024-04-03 21:47:33 -0400

    Pregunta 2. Styling changes

--- Pregunta 2/main.go
+++ Pregunta 2/main.go
@@ -7,70 +7,70 @@ import (
 /*
 Versión modificada de la Criba de Eratóstenes, donde
 en lugar de almacenar dentro de la Criba si un número
-es primo o no, sino su factor primo más pequeño.
+es primo o no, se almacena su factor primo más pequeño.
 */
 func eratosthenesSieve(length int) []int {
-  sieve := make([]int, length)
-  for i := 0; i < len(sieve); i++ {
-    if sieve[i] == 0 {
-      sieve[i] = i+2
-      j := i
-      for j + (i+2) < len(sieve) {
-        j += i+2
-        if sieve[j] == 0 {
-          sieve[j] = i+2
-        }
-      }
-    }
-  }
-  return sieve
+	sieve := make([]int, length)
+	for i := 0; i < len(sieve); i++ {
+		if sieve[i] == 0 {
+			sieve[i] = i + 2
+			j := i
+			for j+(i+2) < len(sieve) {
+				j += i + 2
+				if sieve[j] == 0 {
+					sieve[j] = i + 2
+				}
+			}
+		}
+	}
+	return sieve
 }
 
 /*
 Función que calcula los factores primos de un número dada
 una criba de Eratóstenes pre-calculada. Los factores primos
-son retornados como un mapa donde las llaves son las bases 
+son retornados como un mapa donde las llaves son las bases
 de los factoes y su respectivo valor es el exponente que posee
 la base dentro de la repersentación en factores primos.
 */
 func primeFactors(n int, es []int) map[int]int {
-  pf := make(map[int]int, 0)
+	pf := make(map[int]int, 0)
 
-  for n > 1{
-    sd := es[n-2]
-    _, ok := pf[sd]
-    
-    if !ok {
-      pf[sd] = 1
-    } else {
-      pf[sd] += 1
-    }
-    n /= sd
-  }
+	for n > 1 {
+		sd := es[n-2]
+		_, ok := pf[sd]
 
-  return pf
+		if !ok {
+			pf[sd] = 1
+		} else {
+			pf[sd] += 1
+		}
+		n /= sd
+	}
+
+	return pf
 }
 
 /*
 Implementación de algoritmo para el cómputo de decomp(X)
 */
 func decomp(n int) int64 {
-  accum := int64(0)
-  es := eratosthenesSieve(n)
+	accum := int64(0)
+	es := eratosthenesSieve(n)
 
-  for i := 1; i < n; i++ {
-    pf := primeFactors(i, es)
-    nd := 1
-    for _, v := range pf {
-      nd *= (v+1)
-    }
-    accum += int64(2*nd)
-  }
+	for i := 1; i < n; i++ {
+		pf := primeFactors(i, es)
+		nd := 1
+		for _, v := range pf {
+			nd *= (v + 1)
+		}
+		accum += int64(2 * nd)
+	}
 
-  return accum
+	return accum
 }
 
 func main() {
-  n := 24
-  fmt.Printf("decomp(%d): %d\n", n, decomp(n))
+	n := 24
+	fmt.Printf("decomp(%d): %d\n", n, decomp(n))
 }
